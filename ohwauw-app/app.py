80cc51a47039da3c4a3bb841e7f9f14eca405631
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2025-08-06 13:44:19 +0200

    Update app.py
    add specific wauw!

--- ohwauw-app/app.py
+++ ohwauw-app/app.py
@@ -9,6 +9,15 @@ def ohwauw():
     wauw = random.choice(lines)
     return render_template('homepage.html', wauw=wauw)
 
+@app.route("/wauw", methods = ["GET"])
+def specificwauw():
+    line = request.args.get('wauw', default = -1, type=int)
+    if line = -1:
+        wauw = random.choice(lines)
+    else:
+        wauw = lines[line]
+    return render_template('homepage.html', wauw=wauw)
+
 @app.route("/api/wauw", methods = ["GET"])
 def getwauw():
     wauw = random.choice(lines)
